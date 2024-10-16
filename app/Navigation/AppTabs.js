a260bc6e337bf3b86ce3e60d9757a2cf3ddeb004
Stefan Magardino <smagardino01@manhattan.edu>
2024-10-16 19:47:02 -0400

    restaurant tab fix

--- app/Navigation/AppTabs.js
+++ app/Navigation/AppTabs.js
@@ -5,9 +5,29 @@ import HomeScreen from '../screens/HomeScreen.js';
 import RestaurantsScreen from '../screens/restaurants.js';
 import Map from '../screens/Map';
 import Settings from '../screens/Settings.js';
+import {StatusBar} from 'expo-status-bar';
+import { useState } from  'react';
+import { View, Text, TouchableOpacity, Linking } from 'react-native';
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "../infrastructure/theme/index.js";
+import { RestaurantsContextProvider } from "../services/restaurants/restaurants.context.js";
+
 
 
 const Tab = createBottomTabNavigator();
+
+const wrappedRestaurant = () => {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <RestaurantsContextProvider>
+          <RestaurantsScreen />
+        </RestaurantsContextProvider>
+      </ThemeProvider>
+    </>
+  );
+ };
+ 
 
 const AppTabs = () => (
   <Tab.Navigator
@@ -28,7 +48,7 @@ const AppTabs = () => (
     })}
   >
     <Tab.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
-    <Tab.Screen name="RestaurantsScreen" component={RestaurantsScreen} options={{ headerShown: false }} />
+    <Tab.Screen name="RestaurantsScreen" component={wrappedRestaurant} options={{ headerShown: false }} />
     <Tab.Screen name="Map" component={Map} options={{ headerShown: false }} />
     <Tab.Screen name="Settings" component={Settings} options={{ headerShown: false }} />
   </Tab.Navigator>
